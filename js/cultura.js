5e65de785414bf402c01254084da593c3dedbbc5
André <andrerafaelbatista@gmail.com>
2023-11-29 17:43:03 -0300

    Javascript responsável pela página cultura.html

---
+++ js/cultura.js
@@ -0,0 +1,23 @@
+let btn_revelar = document.querySelectorAll(".revelador")
+let body = document.querySelector("body");
+
+function mostrarConteudo(){
+    let blur = document.createElement('div');
+    let content = document.createElement("div");
+
+    blur.classList.add('blur');
+    content.classList.add("popup");
+
+    content.innerHTML=`
+        <h3>${this.parentElement.querySelector('h3').textContent}</h3>
+        <p>${this.parentElement.querySelector('p').textContent}</p>
+        <button class="revelador" onclick="fecharPopup()">Fechar</button>
+    ` ;
+
+    body.appendChild(blur)
+    blur.appendChild(content)
+}
+function fecharPopup(){
+    document.querySelector('.blur').remove()
+}
+btn_revelar.forEach(btn => btn.addEventListener("click",mostrarConteudo))
